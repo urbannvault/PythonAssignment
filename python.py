9304ca21de76f48cdd7b3fb4ecd37ada06ddf2ec
Innocent Ojwang <159828804+urbannvault@users.noreply.github.com>
2024-03-05 16:07:09 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,24 @@
+#site_name="Aubanvault"
+#print(site_name)
+#site_name= "Aubanchat"
+#print(site_name)
+#num=28
+#print(num)
+#innocent=5
+#print(innocent)
+#site_name="Jessicah"
+#print(site_name)
+#var_num=5
+#print(var_num)
+#print("Good morning")
+#print("Aubanvault is bae")
+#print("Good morning", end=" ")
+#print("Aubanvault is bae")
+#num=input("Enter number: ")
+#print("You Entered:", num)
+#print("Data type of num:", type(num))
+name=input("Enter Your Name: ")
+aubanvault=input("Enter Your Mpesa number as a luggage tracking ID: ")
+location=input("Enter the Vault Location: ")
+duration=input("Enter the Time Duration for using short-term storage ")
+print("Hello my name is:", name, "my Mpesa number is:", aubanvault, "and I'm using this Aubanvault at:", location, "For a time duration of:", duration)
